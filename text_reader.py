26e76598f2f1a3a2ed23156eacd488fbaf6470e0
Emma Cote <emmacote@Emmas-MacBook-Pro.local>
2026-02-21 18:51:41 -0500

    Broke out the functions to be commented out and called in different stages. Technically, it works in that sense but still needs fixing up to get it all to run in one go. That includes the new code that reads the image file using tesseract.

--- text_reader.py
+++ text_reader.py
@@ -6,25 +6,51 @@ to begin with.
 
 And even OCR scanned come with no guarantee.
 There's no guarantee that the OCR embedded in the hardware and/or OS to get everything.
+
+NOTE: 
 """
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PageObject
+from PIL import Image
+import pytesseract
 import sys
 
+pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
+
+def lookup_extractable_text(page: PageObject):
+    extractable_text = page.extract_text()
+    if extractable_text:
+        print(f"extracted text...")
+        print(extractable_text)
+        print(f"length of text found: {len(extractable_text)}")
+    else:
+        print(f"sorry. No extractable text found in this file. :-(")
+
+
+def create_image_file(page: PageObject):
+    image = page.images[0]
+    with open("extracted_image.jpg", "wb") as fp:
+        fp.write(image.data)
+
+    print(f"stop point...")
+
+
+def ocr_scan_extracted_image():
+    file_name = "extracted_image.jpg"
+    string_data = pytesseract.image_to_string("extracted_image.jpg")
+    print(string_data)
 
 
 if __name__ == '__main__':
     print("welcome to my pdf playground (text extractor script)...")
+
     if len(sys.argv) > 1:
+
         file_name = sys.argv[1]
         print(f"working with file: {file_name}")
         reader = PdfReader(file_name)
         print(f"page count: {len(reader.pages)}")
         page = reader.pages[0]
 
-        extractable_text = page.extract_text()
-        if extractable_text:
-            print(f"extracted text...")
-            print(extractable_text)
-            print(f"length of text found: {len(extractable_text)}")
-        else:
-            print(f"sorry. No extractable text found in this file. :-(")
+        #lookup_extractable_text(page)
+        #create_image_file(page)
+        ocr_scan_extracted_image()
